d4d730b45f2d48e05e2317ecc5407335043b18dd
Dmytro <dimabokov5@gmail.com>
2025-02-04 21:20:36 +0000

    Added: *paragraph under main heading *catalog button
    Editted:
    *Main heading

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import "../home.scss"
 
+
 class Home extends React.Component
 {
     constructor(props)
@@ -14,9 +15,17 @@ class Home extends React.Component
         return(
             <div id="hero-container">
                 <div id="hero">
+                    {/* TODO
+                        * Make responsive */}
                     <div id="heading-container">
                         <h1 id="heading">GUITAR HERO</h1>
                     </div>
+                    <div id="paragraph-container">
+                        <h1 id="paragraph">Find the perfect sound with our premium selection of musical instruments. From beginners to professionals, we have everything you need to bring your music to life.</h1>
+                    </div>
+                    {/* TODO
+                        * Make the button an anchor link */}
+                    <button id="catalog-button">CATALOG</button>
                     <video autoPlay muted loop id="background">
                         <source src="../video/background.mp4"/>
                     </video>
