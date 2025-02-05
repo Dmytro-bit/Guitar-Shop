654373831a903099faef427e29c0eb28c737ca1f
Dmytro <dimabokov5@gmail.com>
2025-02-05 17:02:11 +0000

    Done: *Home page rerstructured *Catalog button redesigned
    Added:
    *Hover effect for user icon and logo

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -15,17 +15,19 @@ class Home extends React.Component
         return(
             <div id="hero-container">
                 <div id="hero">
-                    {/* TODO
-                        * Make responsive */}
-                    <div id="heading-container">
-                        <h1 id="heading">GUITAR HERO</h1>
+                    <div id="text-container">
+                        <div id="inner-text-container">
+                            <div id="heading-container">
+                                <h1 id="heading">GUITAR HERO</h1>
+                            </div>
+                            <div id="paragraph-container">
+                                <h1 id="paragraph">Find the perfect sound with our premium selection of musical instruments. From beginners to professionals, we have everything you need to bring your music to life.</h1>
+                            </div>
+                        </div>
+                        {/* TODO
+                            * Make the button an anchor link */}
+                        <div id="catalog-button-container"><button id="catalog-button">CATALOG</button></div>
                     </div>
-                    <div id="paragraph-container">
-                        <h1 id="paragraph">Find the perfect sound with our premium selection of musical instruments. From beginners to professionals, we have everything you need to bring your music to life.</h1>
-                    </div>
-                    {/* TODO
-                        * Make the button an anchor link */}
-                    <button id="catalog-button">CATALOG</button>
                     <video autoPlay muted loop id="background">
                         <source src="../video/background.mp4"/>
                     </video>
