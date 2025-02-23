628241d926e8e1b6d0aa88f77d293c8df488a134
Dmytro Bokov <dimabokov5@gmail.com>
2025-02-23 01:10:02 +0000

    Added: *Filter section (template)

--- client/src/component/Products.js
+++ client/src/component/Products.js
@@ -16,8 +16,36 @@ class Products extends React.Component
     {
         return(
             <><Nav />
-        
-            <div><CardList /></div></>
+
+                <div className="products-container">
+                    <div className="products-controls-container">
+                        <div className="products-controls">
+                            <div className="product-search-container">
+                                <img src="../icons/search-icon.png"  alt="search-icon" className="product-search-icon"/>
+                                <input type="text" className="product-search" placeholder="Search..."/>
+                            </div>
+                            <div className="sort-dropdown">
+                                <input type="checkbox" className="sort-dropdown-toggle" id="sort-dropdown-toggle"/>
+                                <label htmlFor="sort-dropdown-toggle" className="sort-dropdown-label">SORT</label>
+
+                                <ul className="sort-dropdown-menu">
+                                    <li className="sort-dropdown-item">Price: Low to High</li>
+                                    <li className="sort-dropdown-item">Price: High to Low</li>
+                                    <li className="sort-dropdown-item">Rating: Low to High</li>
+                                    <li className="sort-dropdown-item">Rating: High to Low</li>
+                                    <li className="sort-dropdown-item">Alphabeticaly: A-Z</li>
+                                    <li className="sort-dropdown-item">Alphabeticaly: Z-A</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="products-filter-container">
+                        <div className="products-filter"></div>
+                    </div>
+                    <div className="products-category-name">All Products</div> {/*Make it display currently viewed category*/}
+                    <div className="cardlist-container"><CardList /></div>
+                </div>
+            </>
         )
     }
 
