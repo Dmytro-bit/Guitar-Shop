6563c752e6736e3d2490ccf99311d7e630c71f27
vladikslon <vladbrovi@gmail.com>
2025-03-04 22:51:30 +0000

    fixed logout issues

--- client/src/component/Account.js
+++ client/src/component/Account.js
@@ -41,8 +41,13 @@ class Account extends React.Component {
         }
     }
     loadUserData = async () => {
+        const email = localStorage.getItem("email");
+        if (!email || email === "null" || email === "undefined") {
+            console.log("No valid email found; user is not logged in.");
+            return;
+        }
         try {
-            const email = localStorage.getItem("email");
+
             console.log(email)
             const res = await axios.get(`user/getProfile`, {params: {email}})
             console.log(res.data.user)
