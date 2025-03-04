b66d0aba2d5af6fa4b7126d90c8195afc414184c
vladikslon <vladbrovi@gmail.com>
2025-03-04 16:49:56 +0000

    Created user.js in routes directory added:  patch request user/upload uploading image when user clicks on the photo profile  get request filling user profile with data fom db  express middleware for interaction with static files express.static()
     divided name into fname and lname

--- client/src/component/Account.js
+++ client/src/component/Account.js
@@ -6,60 +6,117 @@ import "../styles/account.scss"
 import SERVER_HOST from "../config/global_constants"
 
 class Account extends React.Component {
-    constructor (props)
-    {
+    constructor(props) {
         super(props)
-        
+
         this.state = {
-            user : {
-                fname : "Test",
-                lname : "User",
-                email : "test@test.com",
-                phone : "+353851234567",
-                address : { fline: "Test Address",
-                            sline: "Test Street",
-                            city: "Test City",
-                            county: "Test County",
-                            eircode: "Test Code",
+            user: {
+                fname: "Test",
+                lname: "User",
+                email: "test@test.com",
+                phone: "+353851234567",
+                address: {
+                    fline: "Test Address",
+                    sline: "Test Street",
+                    city: "Test City",
+                    county: "Test County",
+                    eircode: "Test Code",
                 },
-                profilePhotoUrl : "../icons/user.png",                
+                profilePhotoUrl: "../icons/user.png",
             },
-            isEditable : false,
+            selectedFile: null,
+            isEditable: false,
+        }
+        this.imageHandlerRef = React.createRef();
+    }
+
+    componentDidMount = async () => {
+        try {
+            await this.loadUserData()
+        } catch (err) {
+            console.log(err);
         }
     }
-
-    // loadUserData = async () =>
-    // {
-    //     try{
-    //         const res = await axios.get(`${SERVER_HOST}/user`)
-    //         console.log(res.data)
-    //         this.setState({user : res.data})
-    //     }
-    //     catch(err) {
-    //         console.log(err)
-    //     }
-    // }
-    closeAccount = () =>
-    {
-        this.setState({isEditable : false})
+    loadUserData = async () => {
+        try {
+            const email = localStorage.getItem("email");
+            console.log(email)
+            const res = await axios.get(`user/getProfile`, {params: {email}})
+            console.log(res.data)
+            const userData = res.data.user; // use the nested user object
+            this.setState({
+                user: {
+                    fname: userData.fname,
+                    lname: userData.lname,
+                    email: userData.email,
+                    phone: userData.phone,
+                    address: {
+                        fline: userData.address.fline,
+                        sline: userData.address.sline, // corrected sline mapping
+                        city: userData.address.city,
+                        county: userData.address.county,
+                        eircode: userData.address.eircode,
+                    },
+                    profilePhotoUrl: userData.profilePhotoUrl ? userData.profilePhotoUrl : "../icons/user.png"
+                }
+            });
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    closeAccount = () => {
+        this.setState({isEditable: false})
     }
 
-    handleEditMode = () =>
-    {
-        this.setState({isEditable : !this.state.isEditable})
+    handleEditMode = () => {
+        this.setState({isEditable: !this.state.isEditable})
 
         //Write a put request ro update the data
     }
+    clickOnPlaceholder = () => {
+        if (this.imageHandlerRef.current) {
+            this.imageHandlerRef.current.click()
+        }
+
+    }
+    handleFileChange = async (e) => {
+        const file = e.target.files[0]
+        this.setState({selectedFile: file})
+        if (file) {
+            try {
+                const formData = new FormData();
+                const email = localStorage.getItem("email");
+                formData.append('file', file)
+                formData.append('email', email)
+                const res = await axios.patch('user/upload', formData, {headers: {"Content-Type": "multipart/form-data"}})
+                const url = res.data.userPhoto;
+                this.setState(prevState => ({
+                    user: {
+                        ...prevState.user,
+                        profilePhotoUrl: url
+                    }
+                }));
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    }
 
     render() {
-        return(
+        return (
             <div className={`user-account-container ${this.props.isActive ? "active" : "inactive"}`}>
-                <img src="../icons/close.png" onClick={() => {this.props.handleClose(); this.closeAccount()}} className="user-account-close"/>
+                <img src="../icons/close.png" onClick={() => {
+                    this.props.handleClose();
+                    this.closeAccount(this.clickOnPlaceholder)
+                }} className="user-account-close"/>
                 <div className="user-account-header">
                     <div className="user-account-photo">
-                        <img src={this.state.user.profilePhotoUrl} className="user-account-photo"/>
+                        <input id="imageHandler" className={"hidden-input"} ref={this.imageHandlerRef}
+                               onChange={this.handleFileChange} type={"file"}/> <img
+                        src={this.state.user.profilePhotoUrl} onClick={this.clickOnPlaceholder}
+                        className="user-account-photo"/>
                     </div>
-                    <div className="user-account-name" style={{ display : this.state.isEditable ? "none" : "flex" }}>
+                    <div className="user-account-name" style={{display: this.state.isEditable ? "none" : "flex"}}>
                         {this.state.user.fname} {this.state.user.lname}
                     </div>
                 </div>
@@ -69,11 +126,13 @@ class Account extends React.Component {
                         <div>
                             <div className="user-account-cred-container">
                                 <p className="user-account-cred-title"><u>First Name :</u></p>
-                                <input type="text" value={this.state.user.fname} disabled={!this.state.isEditable} className="user-account-cred"></input>
+                                <input type="text" value={this.state.user.fname} disabled={!this.state.isEditable}
+                                       className="user-account-cred"></input>
                             </div>
                             <div className="user-account-cred-container">
                                 <p className="user-account-cred-title"><u>Last Name :</u></p>
-                                <input type="text" value={this.state.user.lname} disabled={!this.state.isEditable} className="user-account-cred"></input>
+                                <input type="text" value={this.state.user.lname} disabled={!this.state.isEditable}
+                                       className="user-account-cred"></input>
                             </div>
                         </div>
                     ) : (
@@ -82,43 +141,58 @@ class Account extends React.Component {
                     )}
                     <div className="user-account-cred-container">
                         <p className="user-account-cred-title"><u>Email :</u></p>
-                        <input type="text" value={this.state.user.email} disabled={!this.state.isEditable} className="user-account-cred"></input>
+                        <input type="text" value={this.state.user.email} disabled={!this.state.isEditable}
+                               className="user-account-cred"></input>
                     </div>
                     <div className="user-account-cred-container">
                         <p className="user-account-cred-title"><u>Phone :</u></p>
-                        <input type="text" value={this.state.user.phone} disabled={!this.state.isEditable} className="user-account-cred"></input>
+                        <input type="text" value={this.state.user.phone} disabled={!this.state.isEditable}
+                               className="user-account-cred"></input>
                     </div>
                     <div className="user-account-cred-container">
-                    {!this.state.isEditable ? (
-                        <div>
-                            <p className="user-account-cred-title"><u>Address :</u></p>
-                            <p className="user-account-cred">
-                                {this.state.user.address.fline},<br/> 
-                                {this.state.user.address.city},<br/> 
-                                {this.state.user.address.county},<br/> 
-                                {this.state.user.address.eircode}
-                            </p>
-                        </div>
+                        {!this.state.isEditable ? (
+                            <div>
+                                <p className="user-account-cred-title"><u>Address :</u></p>
+                                <p className="user-account-cred">
+                                    {this.state.user.address.fline},<br/>
+                                    {this.state.user.address.city},<br/>
+                                    {this.state.user.address.county},<br/>
+                                    {this.state.user.address.eircode}
+                                </p>
+                            </div>
                         ) : (
                             <div>
-                                <p className={`user-account-cred-title ${this.state.isEditable ? "editable" : ""}`}><u>First Address Line :</u></p>
-                                <input type="text" defaultValue={this.state.user.address.fline} className={`user-account-cred ${this.state.isEditable ? "editable" : ""}`}/>
-                                <p className={`user-account-cred-title ${this.state.isEditable ? "editable" : ""}`}><u>Second Address Line :</u></p>
-                                <input type="text" defaultValue={this.state.user.address.sline} className={`user-account-cred ${this.state.isEditable ? "editable" : ""}`}/>
-                                <p className={`user-account-cred-title ${this.state.isEditable ? "editable" : ""}`}><u>City/Town :</u></p>
-                                <input type="text" defaultValue={this.state.user.address.city} className={`user-account-cred ${this.state.isEditable ? "editable" : ""}`}/>
-                                <p className={`user-account-cred-title ${this.state.isEditable ? "editable" : ""}`}><u>County :</u></p>
-                                <input type="text" defaultValue={this.state.user.address.county} className={`user-account-cred ${this.state.isEditable ? "editable" : ""}`}/>
-                                <p className={`user-account-cred-title ${this.state.isEditable ? "editable" : ""}`}><u>Eircode</u></p>
-                                <input type="text" defaultValue={this.state.user.address.eircode} className={`user-account-cred ${this.state.isEditable ? "editable" : ""}`}/>
+                                <p className={`user-account-cred-title ${this.state.isEditable ? "editable" : ""}`}><u>First
+                                    Address Line :</u></p>
+                                <input type="text" defaultValue={this.state.user.address.fline}
+                                       className={`user-account-cred ${this.state.isEditable ? "editable" : ""}`}/>
+                                <p className={`user-account-cred-title ${this.state.isEditable ? "editable" : ""}`}><u>Second
+                                    Address Line :</u></p>
+                                <input type="text" defaultValue={this.state.user.address.sline}
+                                       className={`user-account-cred ${this.state.isEditable ? "editable" : ""}`}/>
+                                <p className={`user-account-cred-title ${this.state.isEditable ? "editable" : ""}`}><u>City/Town
+                                    :</u></p>
+                                <input type="text" defaultValue={this.state.user.address.city}
+                                       className={`user-account-cred ${this.state.isEditable ? "editable" : ""}`}/>
+                                <p className={`user-account-cred-title ${this.state.isEditable ? "editable" : ""}`}><u>County
+                                    :</u></p>
+                                <input type="text" defaultValue={this.state.user.address.county}
+                                       className={`user-account-cred ${this.state.isEditable ? "editable" : ""}`}/>
+                                <p className={`user-account-cred-title ${this.state.isEditable ? "editable" : ""}`}>
+                                    <u>Eircode</u></p>
+                                <input type="text" defaultValue={this.state.user.address.eircode}
+                                       className={`user-account-cred ${this.state.isEditable ? "editable" : ""}`}/>
                             </div>
                         )
-                    }                        
+                        }
                     </div>
                 </div>
                 <div className="user-account-edit-button">
-                    <button className="user-account-edit-save" onClick={this.handleEditMode}>{this.state.isEditable ? "SAVE" : "EDIT"}</button>
-                    <button className="user-account-edit-save" onClick={this.handleEditMode} style={{ display : this.state.isEditable ? "block" : "none" }}>CANCEL</button>
+                    <button className="user-account-edit-save"
+                            onClick={this.handleEditMode}>{this.state.isEditable ? "SAVE" : "EDIT"}</button>
+                    <button className="user-account-edit-save" onClick={this.handleEditMode}
+                            style={{display: this.state.isEditable ? "block" : "none"}}>CANCEL
+                    </button>
                 </div>
             </div>
         )
--- server/models/users.js
+++ server/models/users.js
@@ -1,18 +1,57 @@
 const mongoose = require('mongoose');
+const {MEDIA_DIR} = require("../server");
 
 const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const passwordPattern = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[£!#€$%^&*¬`@=)(-_:;'{}/\\ \[\].,<>?~|]).{8,}$/
 const phonePattern = /^\+3538\d\d{3,4}\d{4}$/;
-const namePattern =/^[a-zA-Z ]{2,30}$/;
+const namePattern = /^[a-zA-Z ]{2,30}$/;
 
 let userSchema = new mongoose.Schema(
     {
-        name: {type: String, required: [true, "Name is required"], match: [namePattern, "Invalid name format"], trim: true},
-        email: {type: String, required: true, unique: [true, "Email is required"], match: [emailPattern, "Invalid email format"],trim: true},
-        password: {type: String, required: [true, "Password is required"], match: [passwordPattern, "Invalid password"]},
-        phone: {type: String, required: [true, "Phone is required"], match: [phonePattern, "Invalid phone format"], trim: true},
-        accessLevel: {type: Number, default: parseInt(process.env.ACCESS_LEVEL_NORMAL_USER), min:[0, "Access level must be a positive number"], max:[2, "Access level does not exist"]},
-        profilePhotoUrl: {type: String, default: ""}
+        fname: {
+            type: String,
+            required: [true, "First name is required"],
+            match: [namePattern, "Invalid name format"],
+            trim: true
+        },
+        lname: {
+            type: String,
+             required: [true, "Last name is required"],
+            match: [namePattern, "Invalid name format"],
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: [true, "Email is required"],
+            match: [emailPattern, "Invalid email format"],
+            trim: true
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            match: [passwordPattern, "Invalid password"]
+        },
+        phone: {
+            type: String,
+            required: [true, "Phone is required"],
+            match: [phonePattern, "Invalid phone format"],
+            trim: true
+        },
+        accessLevel: {
+            type: Number,
+            default: parseInt(process.env.ACCESS_LEVEL_NORMAL_USER),
+            min: [0, "Access level must be a positive number"],
+            max: [2, "Access level does not exist"]
+        },
+        address: {
+            fline: {type: String, default: ""},
+            sline: {type: String, default: ""},
+            city: {type: String, default: ""},
+            county: {type: String, default: ""},
+            eircode: {type: String, default: ""}
+        },
+        profilePhotoUrl: {type: String, get: v => `${MEDIA_DIR}${v}`, default: "./public/icons/user.png"},
     }
 )
 
