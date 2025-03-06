e524fcd0e8bda2cd2f3582b7824129e2d2b0b005
vladikslon <vladbrovi@gmail.com>
2025-03-06 17:30:10 +0000

    Feature: update user profile

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,6 +6,8 @@ const upload = multer({dest: `${process.env.UPLOADED_FILES_FOLDER}`})
 const emptyFolder = require('empty-folder')
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
+const e = require("express");
+const {response} = require("express");
 const JWT_PRIVATE_KEY = fs.readFileSync(process.env.JWT_PRIVATE_KEY, "utf8");
 // upload image to profile
 const uploadImage = (req, res, next) => {
@@ -85,7 +87,6 @@ const verifyLogin = async (req, res, next) => {
     const token = authHeader.split(" ")[1];
     let decoded;
     try {
-        // Synchronously verify the token. This will throw if the token is invalid.
         decoded = jwt.verify(token, JWT_PRIVATE_KEY);
     } catch (err) {
         console.error("Token verification error:", err);
@@ -100,10 +101,9 @@ const verifyLogin = async (req, res, next) => {
     if (expiryDate.getTime() < Date.now()) {
         return res.status(401).json({error: "Token expired"});
     }
-
-
     next();
 }
+
 const returnUserData = async (req, res, next) => {
     try {
         const user = req.user;
@@ -113,9 +113,22 @@ const returnUserData = async (req, res, next) => {
         next(err)
     }
 }
+const updateProfile = async (req, res, next) => {
+    try{
+        const {email,user} = req.body;
 
+        const newUser = await usersModel.findOneAndUpdate({email: email}, user, {returnDocument:`after`})
+
+        res.status(200).json({user})
+    } catch (e) {
+        next(e)
+    }
+
+
+}
 router.get('/getProfile', checkUserExists, verifyLogin, returnUserData);
 router.patch('/upload', upload.single('file'),verifyLogin, uploadImage, addImageToProfile);
 router.patch('/editAddress',verifyLogin, editAddress);
+router.patch('/updateProfile',verifyLogin, updateProfile);
 
 module.exports = router;
