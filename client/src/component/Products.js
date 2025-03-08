27bc883377f43c95355da29d76602648d71f95f7
Dmytro Bokov <dimabokov5@gmail.com>
2025-03-08 01:52:36 +0000

    Added: *Filter basic front-end

--- client/src/component/Products.js
+++ client/src/component/Products.js
@@ -5,11 +5,29 @@ import CardList from "./CardList"
 
 import "../styles/products.scss"
 
+import axios from "axios";
+
 class Products extends React.Component
 {
     constructor(props)
     {
         super(props);
+
+        this.state = {
+            data : {},
+            parameters : {},
+        }
+    }
+
+    componentDidMount = async () =>
+    {
+        try {
+            const res = await axios.get("/products/filter")
+            this.setState({data: res.data.data, parameters : res.data.data.parameters});
+            console.log("Response data:", this.state.parameters);
+        }catch(e){
+            console.log(e.message);
+        }
     }
 
     render()
@@ -50,7 +68,29 @@ class Products extends React.Component
                         </div>
                     </div>
                     <div className="products-filter-container">
-                        <div className="products-filter"></div>
+                        <div className="products-filter">
+                            <div className="products-filter-brands-container">BRANDS</div>
+                            <div className="products-filter-categories-container">CATEGORIES</div>
+                            <div className="products-filter-parameters-container">
+                                <p className="products-filter-parameters-title">PARAMETERS</p>
+                                <div className="filter-parameters-container">
+                                    {Object.keys(this.state.parameters).map((parameter, index) => (
+                                        <div className="filter-parameter-container" key={index}>
+                                            <input type="checkbox" className="filter-parameter-input" id={`filter-parameter-${parameter}`} key={index}/>
+                                            <label htmlFor={`filter-parameter-${parameter}`} className="filter-parameter-title"><b>{parameter}</b></label>
+                                            <div className="filter-parameter-dropdown">
+                                                {this.state.parameters[parameter].map((option, index) => (
+                                                    <div className="filter-parameter-dropdown-option" key={index}>
+                                                        <input type="checkbox" id={`filter-parameter-${option}`} className="filter-parameter-checkbox"/>
+                                                        <label htmlFor={`filter-parameter-${option}`}>{option}</label>
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="products-category-name">All Products</div>
                     {/*Make it display currently viewed category*/}
